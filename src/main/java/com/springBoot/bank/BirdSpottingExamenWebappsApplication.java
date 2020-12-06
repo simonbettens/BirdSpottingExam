f5498cccaf88cf6ad4cb1e6b814b7a93ac04d5f3
Simon <simon.bettens@live.be>
2020-12-06 10:40:17 +0100

    inital commit

---
+++ src/main/java/com/springBoot/bank/BirdSpottingExamenWebappsApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdSpottingExamenWebappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdSpottingExamenWebappsApplication.class, args);
+	}
+
+}
